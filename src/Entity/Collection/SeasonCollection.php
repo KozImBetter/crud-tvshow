9f0b17c9a8d53a9b698c46110d75374dc8c245e6
Sacha Deutsche <sacha.deutsche@etudiant.univ-reims.fr>
2024-06-10 14:16:23 +0200

    modification selon la requete du merge

--- src/Entity/Collection/SeasonCollection.php
+++ src/Entity/Collection/SeasonCollection.php
@@ -9,11 +9,11 @@ use PDO;
 
 class SeasonCollection
 {
-    public static function findAllByTvShowId(int $tvShowId): array
+    public static function findByTvShowId(int $tvShowId): array
     {
         $season = MyPDO::getInstance()->prepare(
             <<<'SQL'
-            SELECT id, name, 
+            SELECT id, name, tvShowId, seasonNumber, posterId
             FROM season
             WHERE tvShowId = :tvShowId
             ORDER BY name
