c87d9c8be3b8d462dcf46e6542416c931e1c58fa
Sacha Deutsche <sacha.deutsche@etudiant.univ-reims.fr>
2024-06-10 16:17:59 +0200

    Modification selon la requete du merge

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -7,9 +7,18 @@ use PDO;
 
 class Genre
 {
+
     private int $id;
     private string $name;
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
+    public function getName(): string
+    {
+        return $this->name;
+    }
     public static function findById(int $genreId) : array {
         $genre = MyPDO::getInstance()->prepare(
             <<<'SQL'
