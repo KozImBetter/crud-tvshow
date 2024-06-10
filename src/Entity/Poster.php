51016e6d9133f2f1063451c7a9dfc69177bc4a27
Sacha Deutsche <sacha.deutsche@etudiant.univ-reims.fr>
2024-06-10 15:22:31 +0200

    Modification de la classe Poster selon la requete du merge

--- src/Entity/Poster.php
+++ src/Entity/Poster.php
@@ -6,4 +6,15 @@ class Poster
 {
     private int $id;
     private string $jpeg;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+
 }
