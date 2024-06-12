d9ffcbaf9f751b50e76f08a87d2dc2aec8fdd59e
Karim Rysman <karim.rysman@etudiant.univ-reims.fr>
2024-06-12 16:44:31 +0200

    script pour gérer les demandes avec les formulaires

--- public/admin/tvShow-form.php
+++ public/admin/tvShow-form.php
@@ -4,11 +4,29 @@ declare(strict_types=1);
 
 use Entity\Exception\EntityNotFoundException;
 use Entity\Exception\ParameterException;
+use Html\Form\TvShowForm;
+use Html\WebPage;
+use Entity\TvShow;
 
 try {
+    if (!isset($_GET['tvShowId']) || !ctype_digit($_GET['tvShowId'])) {
+        $tvShow = new TvShow();
+        $form = new TvShowForm(null);
+        $webPage = new WebPage();
+        $webPage->setTitle("Modifier une série");
+        $webPage->appendContent($form->getHtmlForm("tvShow-save.php"));
+        echo $webPage->toHTML();
+    }
+    $tvShow = TvShow::findById(intval($_GET['tvShowId']));
+    $form = new TvShowForm($tvShow);
+    $webPage = new WebPage();
+    $webPage->setTitle("Modifier une série");
+    $webPage->appendContent($form->getHtmlForm("tvShow-save.php"));
+
+    echo $webPage->toHTML();
 
 } catch (ParameterException) {
-    http_response_code(400);
+    echo "hey";
 } catch (EntityNotFoundException) {
     http_response_code(404);
 } catch (Exception) {
